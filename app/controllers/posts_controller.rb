197bdc5b6f2713d01fb2fdff3c5b12b2198b8dc0
damari <motokin128@gmail.com>
2021-06-07 01:18:01 +0900

    編集、削除画面を実装を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,20 +4,36 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.update(post_params)
+    redirect_to post
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+  end
+
+  private
+  
+
+  def post_params
+    params.require(:post).permit(:title)
   end
 end
